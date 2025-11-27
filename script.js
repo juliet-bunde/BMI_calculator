c7008295514c6277a251841d5c53f90bd94f701e
Juliet Bunde <jbunde@OAKAR.local>
2025-11-27 15:52:40 +0300

    enhance BMI calculation logic

--- script.js
+++ script.js
@@ -7,9 +7,29 @@ function calculateBMI(){
     height = parseFloat(height);
 
     //validate the inputs
-    if(weight==="" || height==="" || weight<=0 || height <=0){
-        document.getElementById('result').innerHTML = "Please enter a valid weight and height.";
+    if( weight<=0 || height <=0){
+        document.getElementById('result').innerText = "Please enter a valid weight and height.";
         return;
     }
-    
+
+    //calculate BMI
+    let BMI = (weight/(height*height));
+    BMI = BMI.toFixed(2);
+
+    //categorize BMI
+    let category = "";
+
+    if(BMI<18.5){
+        category = "underweight";
+    }else if (BMI>=18.5 && BMI < 24.9){
+        category = "normal weight";
+    }else if (BMI >=25 && BMI < 29.9){
+        category = "overweight";
+    }else{
+        category = "obesity";
+    }
+
+    //display the result
+    document.getElementById("result").innerText = `Your BMI is ${BMI}, (${category}).`;
+
 }
